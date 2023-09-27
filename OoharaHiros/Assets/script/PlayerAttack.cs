c17d14591cdb836bf81803d21b6d0beee63e9408
Lambda1130 <saku0120etou@ybb.ne.jp>
2023-09-27 14:57:16 +0900

    up

--- OoharaHiros/Assets/script/PlayerAttack.cs
+++ OoharaHiros/Assets/script/PlayerAttack.cs
@@ -1,55 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
-using UnityEditor;
 using UnityEngine;
+using static Cinemachine.CinemachineTargetGroup;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerAttack : MonoBehaviour
 {
-    //ターゲット
-    public enemywrok Target;
+    GameObject enemy1;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //進行方向
-        Vector2 forward = Target.transform.position - transform.position;
-        //角度を変換する
-        float angle = Mathf.Atan2(forward.x, forward.y) * Mathf.Rad2Deg;
-        //角度を代入
-        transform.rotation=Quaternion.Euler(0,0,angle);
+       
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(!Target)
+       
+
+
+
+        if(transform.position.x<-5.0f)
         {
-            Destroy(gameObject);
-            return;
+            Destroy(enemy1);
         }
 
-        //移動
-        Vector2 forward= Target.transform.position - transform.position;
-        Rigidbody2D.position += forward.normalized * Stats.MoveSpeed * Time.deltaTime;
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<enemywrok>(out var enmey)) return;
-
-        float attack = Stats.
-
-         //ターゲットと衝突
-         if(Target == enmey)
-        {
-            Target = null;
-        }
-         else
-        {
-            attack /= 2;
-        }
-
-         (collision.attack);
+       
     }
 }
